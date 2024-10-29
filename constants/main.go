3e0e72c93bc9b53b10d2ddf1d54142873cab2405
Zaid <me@zaid.pro>
2024-10-29 22:15:03 +0300

    constants

---
+++ constants/main.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main() {
+	// Variables =>> var name type
+	// Constant =>> const
+	const MyConst = "This is constant."
+	fmt.Println(MyConst)
+
+	// You can't change it's value.
+}
